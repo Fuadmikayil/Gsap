f7f9ad9677fcbd3db58dfc3cb334c9c90f3d6486
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-11-23 12:04:36 +0400

    Coffee

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,60 +1,41 @@
-import React, { useEffect, useRef } from "react";
-import gsap from "gsap";
+import React from 'react'
+import Header from './components/header'
+
 const App = () => {
-
-  const logoRef = useRef();
-  const navBtnsRef = useRef()
-  const tl = gsap.timeline({ defaults: { duration: 0.6 } });
-
-  useEffect(() => {
-    tl.fromTo(logoRef.current, { opacity: 0 ,x:-30 ,y:-20,scale:0.5}, { opacity: 1 ,x:0,y:0,scale:1});
-    tl.fromTo(navBtnsRef.current, { opacity: 0 ,x:30,y:-20,scale:0.5}, { opacity: 1,x:0 ,y:0,scale:1},"<");
-
-  }, []);
-
   return (
     <div>
-      <header className="flex items-center justify-between py-4 px-8 bg-black text-white">
-        <h1
-          ref={logoRef}
-          className='text-green-400  text-5xl italic tracking-wider font-["Montserrat"]'
-        >
-          GSAP
-        </h1>
-        <nav className="space-x-5">
-          <a
-            href="#"
-            className=" text-gray-400 tracking-wide font-medium text-lg border-b-2 border-b-transparent transition duration-300 hover:text-white hover:border-b-white"
-          >
-            About
-          </a>
-          <a
-            href="#"
-            className=" text-gray-400 tracking-wide font-medium text-lg border-b-2 border-b-transparent transition duration-300 hover:text-white hover:border-b-white"
-          >
-            Contact
-          </a>
-          <a
-            href="#"
-            className=" text-gray-400 tracking-wide font-medium text-lg border-b-2 border-b-transparent transition duration-300 hover:text-white hover:border-b-white"
-          >
-            Services
-          </a>
-          <a
-            href="#"
-            className=" text-gray-400 tracking-wide font-medium  text-lg border-b-2 border-b-transparent transition duration-300 hover:text-white hover:border-b-white"
-            xt-l
-          >
-            Products
-          </a>
-        </nav>
-        <div className="space-x-3" ref={navBtnsRef}>
-          <button className="text-gray-400">Login</button>
-          <button>Sign Up</button>
+      <div className=" min-h-screen flex flex-col">
+        <Header />
+        <div className="flex-1 bg-teal-950 text-white py-40 flex flex-col items-center justify-center gap-8">
+          <h1 className="text-center text-6xl max-w-6xl font-bold tracking-wider leading-[120%]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className="text-center text-gray-400 text-2xl max-w-6xl font-light tracking-wider leading-[120%]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+            ex. Rem consectetur atque, aliquid sunt minus perferendis temporibus
+            similique soluta autem beatae illum consequatur debitis officiis
+            non? Tempore, architecto aperiam!
+          </p>
+          <button className="bg-teal-900 p-6 rounded-xl transition duration-300 text-2xl font-bold hover:bg-green-600">
+            Buy One Cup Of Coffee
+          </button>
         </div>
-      </header>
+      </div>
+
+      <div className="min-h-screen bg-teal-800 py-40">
+        <div className="container mx-auto flex items-center justify-center gap-24">
+          <div className='w-full h-full max-w-xl'>
+            <img className='w-full h-full object-cover' src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/640px-Starbucks_Corporation_Logo_2011.svg.png" />
+          </div>
+          <div className=' text-white flex flex-col gap-8'>
+            <h2 className="text-4xl  font-bold tracking-wider leading-[120%]">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            <p className=" text-gray-200 text-2xl font-normal tracking-wider leading-[120%]"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis cum quia nobis sed beatae incidunt.</p>
+            <p className=" text-gray-200 text-2xl font-normal tracking-wider leading-[120%]"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis cum quia nobis sed beatae incidunt.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
-export default App;
+export default App
